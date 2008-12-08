a3c178a641bffaccecb2c65e6a03578de6ad135a
dschadow <dschadow>
2008-12-08 20:39:55 +0000

    added Activator class

---
+++ sourceediting/plugins/org.eclipse.wst.xml.security.ui/src/org/eclipse/wst/xml/security/ui/internal/XSTUIPlugin.java
@@ -0,0 +1,21 @@
+package org.eclipse.wst.xml.security.ui.internal;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class XSTUIPlugin extends AbstractUIPlugin {
+
+    public XSTUIPlugin() {
+    }
+
+    @Override
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+    }
+
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        super.stop(context);
+    }
+
+}

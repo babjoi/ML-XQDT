2530f5172096f9ac7de4adf3ac7627312d831eda
dschadow <dschadow>
2009-04-05 13:15:56 +0000

    Refactoring for cheat sheet classes

--- sourceediting/plugins/org.eclipse.wst.xml.security.core/src/org/eclipse/wst/xml/security/core/cheatsheets/CreateSampleProject.java
+++ sourceediting/plugins/org.eclipse.wst.xml.security.core/src/org/eclipse/wst/xml/security/core/cheatsheets/CreateSampleProject.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008 Dominik Schadow - http://www.xml-sicherheit.de
+ * Copyright (c) 2009 Dominik Schadow - http://www.xml-sicherheit.de
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,7 +15,6 @@ import java.io.ByteArrayInputStream;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
@@ -23,8 +22,6 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.dialogs.ErrorDialog;
-import org.eclipse.swt.widgets.Shell;
-import org.eclipse.ui.IEditorDescriptor;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.cheatsheets.ICheatSheetAction;
@@ -32,16 +29,16 @@ import org.eclipse.ui.cheatsheets.ICheatSheetManager;
 import org.eclipse.ui.part.FileEditorInput;
 import org.eclipse.wst.xml.security.core.XmlSecurityPlugin;
 
-
 /**
- * <p>Cheat Sheet action. Creates a new project (if it doesn't exist) and a new XML
- * document (if it doesn't exist) containing the sample XML document structure
- * from the help pages.</p>
+ * <p>Cheat Sheet action to create the sample project. Creates a new project
+ * named <b>XML-Security</b> (if it doesn't exist yet) and a new XML
+ * document named <b>FirstSteps.xml</b> (if it doesn't exist yet) containing
+ * the sample XML document structure from the help pages.</p>
  *
  * @author Dominik Schadow
  * @version 0.5.0
  */
-public class CreateProjectFile extends Action implements ICheatSheetAction {
+public class CreateSampleProject extends Action implements ICheatSheetAction {
     /** The example project to create. */
     private static final String EXAMPLE_PROJECT = "XML-Security"; //$NON-NLS-1$
     /** The example XML document to create. */
@@ -67,21 +64,21 @@ public class CreateProjectFile extends Action implements ICheatSheetAction {
             + "   </FinancialInstitutionBranch>\n" //$NON-NLS-1$
             + "  </PayeeFinancialAccount>\n" //$NON-NLS-1$
             + " </PaymentMeans>\n" //$NON-NLS-1$
-            + " <TotalAmount currencyID=\"€\">382.00</TotalAmount>\n" //$NON-NLS-1$
+            + " <TotalAmount currencyID=\"EUR\">382.00</TotalAmount>\n" //$NON-NLS-1$
             + " <!-- item 1 -->\n" //$NON-NLS-1$
             + " <InvoiceLine id=\"1\">\n" //$NON-NLS-1$
             + "  <Quantity>2</Quantity>\n" //$NON-NLS-1$
-            + "  <LineAmount currencyID=\"€\">205.00</LineAmount>\n" //$NON-NLS-1$
+            + "  <LineAmount currencyID=\"EUR\">205.00</LineAmount>\n" //$NON-NLS-1$
             + "  <Item id=\"236WV\">\n" //$NON-NLS-1$
-            + "   <BasePrice currencyID=\"€\">102.50</BasePrice>\n" //$NON-NLS-1$
+            + "   <BasePrice currencyID=\"EUR\">102.50</BasePrice>\n" //$NON-NLS-1$
             + "  </Item>\n" //$NON-NLS-1$
             + " </InvoiceLine>\n" //$NON-NLS-1$
             + " <!-- item 2 -->\n" //$NON-NLS-1$
             + " <InvoiceLine id=\"2\">\n" //$NON-NLS-1$
             + "  <Quantity>1</Quantity>\n" //$NON-NLS-1$
-            + "  <LineAmount currencyID=\"€\">177.00</LineAmount>\n" //$NON-NLS-1$
+            + "  <LineAmount currencyID=\"EUR\">177.00</LineAmount>\n" //$NON-NLS-1$
             + "  <Item id=\"193DX\">\n" //$NON-NLS-1$
-            + "   <BasePrice currencyID=\"€\">177.00</BasePrice>\n" //$NON-NLS-1$
+            + "   <BasePrice currencyID=\"EUR\">177.00</BasePrice>\n" //$NON-NLS-1$
             + "  </Item>\n" //$NON-NLS-1$
             + " </InvoiceLine>\n" //$NON-NLS-1$
             + "</Invoice>"); //$NON-NLS-1$
@@ -95,15 +92,14 @@ public class CreateProjectFile extends Action implements ICheatSheetAction {
     public void run(final String[] params, final ICheatSheetManager manager) {
         try {
             IWorkspace workspace = ResourcesPlugin.getWorkspace();
-            IWorkspaceRoot root = workspace.getRoot();
-            IProject sampleProject = root.getProject(EXAMPLE_PROJECT);
+            IProject sampleProject = workspace.getRoot().getProject(EXAMPLE_PROJECT);
 
-            // Create project if it does not yet exist
+            // create project if it does not yet exist
             if (!sampleProject.exists()) {
                 sampleProject.create(null);
             }
 
-            // Open the project if it is closed
+            // open the project if it is closed
             if (!sampleProject.isOpen()) {
                 sampleProject.open(null);
             }
@@ -113,15 +109,22 @@ public class CreateProjectFile extends Action implements ICheatSheetAction {
             IWorkbench workbench = PlatformUI.getWorkbench();
 
             if (!sampleFile.exists()) {
-                // Create the new file and fill it with the sample content
+                // create the new file and fill it with the sample content
                 sampleFile.create(new ByteArrayInputStream(FIRST_STEPS.toString().getBytes("UTF-8")), true, null);
             }
 
-            IEditorDescriptor desc = workbench.getEditorRegistry().getDefaultEditor(sampleFile.getName());
             workbench.getActiveWorkbenchWindow().getActivePage().openEditor(new FileEditorInput(sampleFile),
-                    desc.getId());
+                    workbench.getEditorRegistry().getDefaultEditor(sampleFile.getName()).getId(), true);
         } catch (Exception ex) {
-            showErrorDialog(Messages.error, Messages.createProjectFileXMLError, ex);
+            String reason = ex.getMessage();
+            if (reason == null) {
+                reason = Messages.errorReasonUnavailable;
+            }
+
+            IStatus status = new Status(IStatus.ERROR, XmlSecurityPlugin.getDefault().getBundle().getSymbolicName(), 0, reason, ex);
+            XmlSecurityPlugin.getDefault().getLog().log(status);
+
+            showErrorDialog(Messages.error, Messages.createProjectFileXMLError, reason, status);
         }
     }
 
@@ -130,22 +133,15 @@ public class CreateProjectFile extends Action implements ICheatSheetAction {
      * information. The error message and error details are automatically
      * written to the workspace log file.
      *
-     * @param title The title of the error dialog
-     * @param message The message of the error dialog
+     * @param title The title for this error dialog
+     * @param message The message for this error dialog
+     * @param reason The detailed error message for this error dialog
+     * @param status The IStatus object for this error dialog
      * @param ex The exception
      */
-    protected void showErrorDialog(final String title, final String message, final Exception ex) {
-        String localizedMessage = ex.getLocalizedMessage();
-        if (localizedMessage == null) {
-            localizedMessage = Messages.errorReasonUnavailable;
-        }
-
-        XmlSecurityPlugin plugin = XmlSecurityPlugin.getDefault();
-        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell();
-        IStatus status = new Status(IStatus.ERROR, plugin.getBundle().getSymbolicName(), 0, localizedMessage, ex);
-        plugin.getLog().log(status);
-
-        ErrorDialog errorDialog = new ErrorDialog(shell, title, message, status, IStatus.ERROR);
+    private void showErrorDialog(final String title, final String message, final String reason, final IStatus status) {
+        ErrorDialog errorDialog = new ErrorDialog(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+                title, message, status, IStatus.ERROR);
         errorDialog.open();
     }
 }

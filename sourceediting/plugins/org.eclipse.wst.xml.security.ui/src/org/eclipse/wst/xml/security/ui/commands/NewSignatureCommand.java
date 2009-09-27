45231d3020d8fa229d8f10ebb915a772c56351db
dschadow <dschadow>
2009-09-27 10:01:01 +0000

    First part to replace actions with commands

---
+++ sourceediting/plugins/org.eclipse.wst.xml.security.ui/src/org/eclipse/wst/xml/security/ui/commands/NewSignatureCommand.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2009 Dominik Schadow - http://www.xml-sicherheit.de
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Dominik Schadow - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.xml.security.ui.commands;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+
+public class NewSignatureCommand extends AbstractHandler {
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}

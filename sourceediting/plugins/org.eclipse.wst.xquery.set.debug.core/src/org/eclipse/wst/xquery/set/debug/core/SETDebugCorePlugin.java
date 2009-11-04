d09185a4388fa672288979190753b78cc2557d90
gpetrovay <gpetrovay>
2009-11-04 15:30:54 +0000

    Incubator: renamed org.eclipse.wst.xqdt.set.debug.core plug-in to org.eclipse.wst.xquery.set.debug.core

---
+++ sourceediting/plugins/org.eclipse.wst.xquery.set.debug.core/src/org/eclipse/wst/xquery/set/debug/core/SETDebugCorePlugin.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright (c) 2008, 2009 28msec Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Gabriel Petrovay (28msec) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.wst.xquery.set.debug.core;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class SETDebugCorePlugin extends Plugin {
+
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.eclipse.wst.xquery.set.debug.core";
+
+    // The shared instance
+    private static SETDebugCorePlugin plugin;
+
+    /**
+     * The constructor
+     */
+    public SETDebugCorePlugin() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static SETDebugCorePlugin getDefault() {
+        return plugin;
+    }
+
+}

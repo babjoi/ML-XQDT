befa17205ffc02072b9575d0ffd1329e8af479fa
sneth <sneth>
2009-12-22 06:26:42 +0000

    - add support for both positive and negative tests - add support for tests with specific language level - add tests for bug 297366 (will fail until fix is checked in)

--- sourceediting/tests/org.eclipse.wst.xquery.core.tests/src/org/eclipse/wst/xquery/internal/core/tests/TestParser.java
+++ sourceediting/tests/org.eclipse.wst.xquery.core.tests/src/org/eclipse/wst/xquery/internal/core/tests/TestParser.java
@@ -14,37 +14,58 @@ import junit.framework.TestCase;
 
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.RecognitionException;
+import org.eclipse.wst.xquery.core.IXQDTLanguageConstants;
 import org.eclipse.wst.xquery.internal.core.parser.antlr.NewLazyTokenStream;
 import org.eclipse.wst.xquery.internal.core.parser.antlr.XQDTCommonTreeAdaptor;
 import org.eclipse.wst.xquery.internal.core.parser.antlr.XQueryLexer;
 import org.eclipse.wst.xquery.internal.core.parser.antlr.XQueryParser;
 
 @SuppressWarnings("restriction")
-public class TestParser extends TestCase {
+public class TestParser extends TestCase implements IXQDTLanguageConstants {
 
     // *****************************************************
     // ****************** TESTS ****************************
     // *****************************************************
 
+    // https://bugs.eclipse.org/bugs/show_bug.cgi?id=297366
+    public void testBug297366_1() {
+        testQuery("declare private variable $bar := ''; ()", false, LANGUAGE_XQUERY);
+    }
+
+    // https://bugs.eclipse.org/bugs/show_bug.cgi?id=297366
+    public void testBug297366_2() {
+        testQuery("declare private variable $bar := ''; ()", true, LANGUAGE_XQUERY_MARK_LOGIC);
+    }
+
+    // https://bugs.eclipse.org/bugs/show_bug.cgi?id=297366
+    public void testBug297366_3() {
+        testQuery("declare private function foo() as xs:string { 'bar' }; ()", false, LANGUAGE_XQUERY);
+    }
+
+    // https://bugs.eclipse.org/bugs/show_bug.cgi?id=297366
+    public void testBug297366_4() {
+        testQuery("declare private function foo() as xs:string { 'bar' }; ()", true, LANGUAGE_XQUERY_MARK_LOGIC);
+    }
+
     // https://bugs.eclipse.org/bugs/show_bug.cgi?id=296671 
     public void testBug296671() {
-        testQuery("\"<\"");
+        testQuery("\"<\"", true, LANGUAGE_XQUERY);
     }
 
     // https://bugs.eclipse.org/bugs/show_bug.cgi?id=295437
     public void testBug295437() {
-        testQuery("<e>{.}--</e>");
+        testQuery("<e>{.}--</e>", true, LANGUAGE_XQUERY);
     }
 
     // *****************************************************
     // *****************************************************
 
-    private void testQuery(String query) {
+    private void testQuery(String query, boolean valid, int languageLevel) {
         XQueryParser parser = prepareParser("parser_unit_tests.xq", query.toCharArray());
+        parser.setLanguageLevel(languageLevel);
         try {
             parser.p_Module();
-            // test for 0 syntax errors 
-            assertEquals(0, parser.getNumberOfSyntaxErrors());
+            assertEquals(valid, parser.getNumberOfSyntaxErrors() == 0);
         } catch (RecognitionException e) {
             // test for RecognitionException 
             assertFalse(e.getMessage(), true);

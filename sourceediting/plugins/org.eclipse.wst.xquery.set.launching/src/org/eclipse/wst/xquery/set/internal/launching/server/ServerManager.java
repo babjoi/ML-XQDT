b6e8d7c064083acefb1a47d9a79a43d0806c7855
gpetrovay <gpetrovay>
2010-06-21 10:36:58 +0000

    Rollback the rollforward

--- sourceediting/plugins/org.eclipse.wst.xquery.set.launching/src/org/eclipse/wst/xquery/set/internal/launching/server/ServerManager.java
+++ sourceediting/plugins/org.eclipse.wst.xquery.set.launching/src/org/eclipse/wst/xquery/set/internal/launching/server/ServerManager.java
@@ -220,11 +220,7 @@ public class ServerManager {
 
     public static int getServerPid(IProject project) throws DebugException {
         try {
-            String toKill = "/test/log/httpd.pid";
-            if (!Platform.getOS().equals(Platform.OS_WIN32)) {
-                toKill = "/test/sausalito.pid";
-            }
-            File pidFile = new File(project.getLocation().toOSString(), toKill);
+            File pidFile = new File(project.getLocation().toOSString(), "/test/log/httpd.pid");
             if (!pidFile.exists()) {
                 throw new FileNotFoundException();
             }

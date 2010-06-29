7d0613008368737d9dd8d5ecd99c530b3e5fdbf8
gpetrovay <gpetrovay>
2010-06-29 17:59:44 +0000

    Updated to Sausalito version 1.0.14

--- sourceediting/plugins/org.eclipse.wst.xquery.set.launching/src/org/eclipse/wst/xquery/set/internal/launching/variables/CoreSdkVersionResolver.java
+++ sourceediting/plugins/org.eclipse.wst.xquery.set.launching/src/org/eclipse/wst/xquery/set/internal/launching/variables/CoreSdkVersionResolver.java
@@ -7,7 +7,7 @@ import org.eclipse.core.variables.IDynamicVariableResolver;
 public class CoreSdkVersionResolver implements IDynamicVariableResolver {
 
     public static final String VARIABLE = "${sdk_version}";
-    public static final String VERSION = "1.0.12";
+    public static final String VERSION = "1.0.14";
 
     public String resolveValue(IDynamicVariable variable, String argument) throws CoreException {
         return VERSION;

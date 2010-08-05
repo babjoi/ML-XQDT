5b3f1463ac444d55e79f7c3f1659bc2394fba38b
Gabriel Petrovay <gabipetrovay@gmail.com>
2010-08-05 23:55:33 +0200

    Formatting only

--- sourceediting/plugins/org.eclipse.wst.xquery.sse.ui/src/org/eclipse/wst/xquery/sse/ui/internal/style/XQueryLineStyleProvider.java
+++ sourceediting/plugins/org.eclipse.wst.xquery.sse.ui/src/org/eclipse/wst/xquery/sse/ui/internal/style/XQueryLineStyleProvider.java
@@ -81,111 +81,111 @@ public class XQueryLineStyleProvider extends AbstractLineStyleProvider implement
     final public static String CK_XML_COMMENT = "XMLComment";
 
     /** Maps region type into color key */
-    final private static Map<String, String> colorKeys = new HashMap<String, String>();
+    final private static Map<String, String> COLOR_KEYS = new HashMap<String, String>();
 
     static {
         // String literal
-        colorKeys.put(XQueryRegions.STRINGLITERAL, CK_STRING_LITERAL);
-        colorKeys.put(XQueryRegions.URILITERAL, CK_STRING_LITERAL);
+        COLOR_KEYS.put(XQueryRegions.STRINGLITERAL, CK_STRING_LITERAL);
+        COLOR_KEYS.put(XQueryRegions.URILITERAL, CK_STRING_LITERAL);
 
         // Comment
-        colorKeys.put(XQueryRegions.XQUERY_COMMENT, CK_COMMENT);
+        COLOR_KEYS.put(XQueryRegions.XQUERY_COMMENT, CK_COMMENT);
 
         // Var ref
-        colorKeys.put(XQueryRegions.VARREF, CK_VAR_REF);
+        COLOR_KEYS.put(XQueryRegions.VARREF, CK_VAR_REF);
 
         // Function call
-        colorKeys.put(XQueryRegions.FUNCTIONNAME, CK_FUNCTIONCALL);
+        COLOR_KEYS.put(XQueryRegions.FUNCTIONNAME, CK_FUNCTIONCALL);
 
         // Undefined region
-        colorKeys.put(XQueryRegions.UNDEFINED, CK_UNDEFINED);
+        COLOR_KEYS.put(XQueryRegions.UNDEFINED, CK_UNDEFINED);
 
         // Types
-        colorKeys.put(XQueryRegions.ST_ATOMICTYPE, CK_TYPE);
-        colorKeys.put(XQueryRegions.ST_EMPTY, CK_TYPE);
-        colorKeys.put(XQueryRegions.ST_ITEM, CK_TYPE);
-        colorKeys.put(XQueryRegions.KT_ANYKINDTEST, CK_TYPE);
-        colorKeys.put(XQueryRegions.KT_ATTRIBUTETEST, CK_TYPE);
-        colorKeys.put(XQueryRegions.KT_COMMENTTEST, CK_TYPE);
-        colorKeys.put(XQueryRegions.KT_DOCUMENTTEST, CK_TYPE);
-        colorKeys.put(XQueryRegions.KT_ELEMENTTEST, CK_TYPE);
-        colorKeys.put(XQueryRegions.KT_PITEST, CK_TYPE);
-        colorKeys.put(XQueryRegions.KT_SCHEMAATTRIBUTETEST, CK_TYPE);
-        colorKeys.put(XQueryRegions.KT_SCHEMAELEMENTTEST, CK_TYPE);
-        colorKeys.put(XQueryRegions.KT_TEXTTEST, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.ST_ATOMICTYPE, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.ST_EMPTY, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.ST_ITEM, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.KT_ANYKINDTEST, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.KT_ATTRIBUTETEST, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.KT_COMMENTTEST, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.KT_DOCUMENTTEST, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.KT_ELEMENTTEST, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.KT_PITEST, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.KT_SCHEMAATTRIBUTETEST, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.KT_SCHEMAELEMENTTEST, CK_TYPE);
+        COLOR_KEYS.put(XQueryRegions.KT_TEXTTEST, CK_TYPE);
 
         // Operators
 
-        colorKeys.put(XQueryRegions.OP_CASTAS, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_OR, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_IDIV, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_DIV, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_PLUS, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_MINUS, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_MULTIPLY, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_GLT, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_GLTE, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_GGT, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_GGTE, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_GEQ, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_GNEQ, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_LT, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_LTE, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_GT, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_GTE, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_EQ, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_NEQ, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_IS, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_BEFORE, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_AFTER, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_TREATAS, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_TO, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_EXCEPT, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_UNION, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_AS, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_OF, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_MOD, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_INSTANCEOF, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_AND, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_INTERSECT, CK_OPERATOR);
-        colorKeys.put(XQueryRegions.OP_CASTABLEAS, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_CASTAS, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_OR, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_IDIV, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_DIV, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_PLUS, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_MINUS, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_MULTIPLY, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_GLT, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_GLTE, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_GGT, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_GGTE, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_GEQ, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_GNEQ, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_LT, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_LTE, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_GT, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_GTE, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_EQ, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_NEQ, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_IS, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_BEFORE, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_AFTER, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_TREATAS, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_TO, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_EXCEPT, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_UNION, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_AS, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_OF, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_MOD, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_INSTANCEOF, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_AND, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_INTERSECT, CK_OPERATOR);
+        COLOR_KEYS.put(XQueryRegions.OP_CASTABLEAS, CK_OPERATOR);
 
         // XML
-        colorKeys.put(XQueryRegions.XML_TAG_OPEN, CK_XML_TAG_DELIMITER);
-        colorKeys.put(XQueryRegions.XML_TAG_CLOSE, CK_XML_TAG_DELIMITER);
-        colorKeys.put(XQueryRegions.XML_EMPTY_TAG_CLOSE, CK_XML_TAG_DELIMITER);
-        colorKeys.put(XQueryRegions.XML_EMPTY_TAG_CLOSE, CK_XML_TAG_DELIMITER);
-        colorKeys.put(XQueryRegions.XML_END_TAG_OPEN, CK_XML_TAG_DELIMITER);
+        COLOR_KEYS.put(XQueryRegions.XML_TAG_OPEN, CK_XML_TAG_DELIMITER);
+        COLOR_KEYS.put(XQueryRegions.XML_TAG_CLOSE, CK_XML_TAG_DELIMITER);
+        COLOR_KEYS.put(XQueryRegions.XML_EMPTY_TAG_CLOSE, CK_XML_TAG_DELIMITER);
+        COLOR_KEYS.put(XQueryRegions.XML_EMPTY_TAG_CLOSE, CK_XML_TAG_DELIMITER);
+        COLOR_KEYS.put(XQueryRegions.XML_END_TAG_OPEN, CK_XML_TAG_DELIMITER);
 
-        colorKeys.put(XQueryRegions.XML_PE_REFERENCE, CK_XML_ENTITY_REFERENCE);
+        COLOR_KEYS.put(XQueryRegions.XML_PE_REFERENCE, CK_XML_ENTITY_REFERENCE);
 
-        colorKeys.put(XQueryRegions.XML_TAG_ATTRIBUTE_EQUALS, CK_XML_ATTR_EQUAL);
+        COLOR_KEYS.put(XQueryRegions.XML_TAG_ATTRIBUTE_EQUALS, CK_XML_ATTR_EQUAL);
 
-        colorKeys.put(XQueryRegions.XML_ATTR_APOS, CK_XML_ATTR_VALUE);
-        colorKeys.put(XQueryRegions.XML_ATTR_QUOT, CK_XML_ATTR_VALUE);
-        colorKeys.put(XQueryRegions.XML_ESCAPE_QUOT, CK_XML_ATTR_VALUE);
-        colorKeys.put(XQueryRegions.XML_ESCAPE_APOS, CK_XML_ATTR_VALUE);
-        colorKeys.put(XQueryRegions.XML_ATTR_CHAR, CK_XML_ATTR_VALUE);
-        colorKeys.put(XQueryRegions.XML_END_ATTR_VALUE, CK_XML_ATTR_VALUE);
+        COLOR_KEYS.put(XQueryRegions.XML_ATTR_APOS, CK_XML_ATTR_VALUE);
+        COLOR_KEYS.put(XQueryRegions.XML_ATTR_QUOT, CK_XML_ATTR_VALUE);
+        COLOR_KEYS.put(XQueryRegions.XML_ESCAPE_QUOT, CK_XML_ATTR_VALUE);
+        COLOR_KEYS.put(XQueryRegions.XML_ESCAPE_APOS, CK_XML_ATTR_VALUE);
+        COLOR_KEYS.put(XQueryRegions.XML_ATTR_CHAR, CK_XML_ATTR_VALUE);
+        COLOR_KEYS.put(XQueryRegions.XML_END_ATTR_VALUE, CK_XML_ATTR_VALUE);
 
-        colorKeys.put(XQueryRegions.XML_TAG_ATTRIBUTE_NAME, CK_XML_ATTR_NAME);
+        COLOR_KEYS.put(XQueryRegions.XML_TAG_ATTRIBUTE_NAME, CK_XML_ATTR_NAME);
 
-        colorKeys.put(XQueryRegions.XML_CDATA, CK_XML_CDATA_CONTENT);
+        COLOR_KEYS.put(XQueryRegions.XML_CDATA, CK_XML_CDATA_CONTENT);
 
-        colorKeys.put(XQueryRegions.XML_PI, CK_XML_PI_CONTENT);
+        COLOR_KEYS.put(XQueryRegions.XML_PI, CK_XML_PI_CONTENT);
 
-        colorKeys.put(XQueryRegions.XML_COMMENT, CK_XML_COMMENT);
+        COLOR_KEYS.put(XQueryRegions.XML_COMMENT, CK_XML_COMMENT);
 
-        colorKeys.put(XQueryRegions.XML_TAG_NAME, CK_XML_TAG_NAME);
+        COLOR_KEYS.put(XQueryRegions.XML_TAG_NAME, CK_XML_TAG_NAME);
 
-        colorKeys.put(XQueryRegions.XML_ELEM_CONTENT_CHAR, CK_XML_CONTENT);
-        colorKeys.put(XQueryRegions.XML_CHAR_REF, CK_XML_CONTENT);
+        COLOR_KEYS.put(XQueryRegions.XML_ELEM_CONTENT_CHAR, CK_XML_CONTENT);
+        COLOR_KEYS.put(XQueryRegions.XML_CHAR_REF, CK_XML_CONTENT);
 
         // Pragma
-        colorKeys.put(XQueryRegions.PRAGMACONTENT, CK_PRAGMA);
-        colorKeys.put(XQueryRegions.PRAGMAQNAME, CK_PRAGMA);
-        colorKeys.put(XQueryRegions.LPRAGMA, CK_PRAGMA);
-        colorKeys.put(XQueryRegions.RPRAGMA, CK_PRAGMA);
+        COLOR_KEYS.put(XQueryRegions.PRAGMACONTENT, CK_PRAGMA);
+        COLOR_KEYS.put(XQueryRegions.PRAGMAQNAME, CK_PRAGMA);
+        COLOR_KEYS.put(XQueryRegions.LPRAGMA, CK_PRAGMA);
+        COLOR_KEYS.put(XQueryRegions.RPRAGMA, CK_PRAGMA);
     }
 
     // Constructors
@@ -210,7 +210,7 @@ public class XQueryLineStyleProvider extends AbstractLineStyleProvider implement
         if (xregion.isKeyword()) {
             colorKey = CK_KEYWORD;
         } else {
-            colorKey = colorKeys.get(region.getType());
+            colorKey = COLOR_KEYS.get(region.getType());
         }
 
         if (colorKey != null) {

5f26242b333f52b5f6d6bde2663a1e3cba77888e
Gabriel Petrovay <gabipetrovay@gmail.com>
2010-08-23 02:00:36 +0200

    [321741] Disabled for now pair matching Added plugin.properties to XQDT SSE plugins Added access restriction inside for all internal eclipse.wst plugins. A couple of file renamings.

---
+++ sourceediting/plugins/org.eclipse.wst.xquery.sse.core/src/org/eclipse/wst/xquery/sse/core/internal/document/XQDTDocumentPartitioner.java
@@ -0,0 +1,54 @@
+/*******************************************************************************
+ * Copyright (c) 2005, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Lionel Villard (IBM) - initial API and implementation
+ *     Gabriel Petrovay (28msec) - added XQuery comment and string partitions
+ *******************************************************************************/
+package org.eclipse.wst.xquery.sse.core.internal.document;
+
+import org.eclipse.wst.sse.core.internal.provisional.text.ITextRegion;
+import org.eclipse.wst.sse.core.internal.text.rules.StructuredTextPartitioner;
+import org.eclipse.wst.xquery.sse.core.internal.regions.XQueryRegions;
+import org.eclipse.wst.xquery.sse.core.text.IXQDTPartitions;
+
+/**
+ * XQuery document partitioner.
+ */
+public class XQDTDocumentPartitioner extends StructuredTextPartitioner implements IXQDTPartitions {
+
+    private final static String[] CONFIGURED_CONTENT_TYPES = new String[] { XQUERY_DEFAULT, XQUERY_COMMENT,
+            XQUERY_STRING };
+
+    // Overrides
+
+    @Override
+    protected void initLegalContentTypes() {
+        fSupportedTypes = CONFIGURED_CONTENT_TYPES;
+    }
+
+    @Override
+    public String getDefaultPartitionType() {
+        return XQUERY_DEFAULT;
+    }
+
+    @Override
+    public String getPartitionType(ITextRegion region, int offset) {
+        String result = null;
+
+        if (region.getType().equals(XQueryRegions.STRINGLITERAL)) {
+            result = XQUERY_STRING;
+        } else if (region.getType().equals(XQueryRegions.XQUERY_COMMENT)) {
+            result = XQUERY_COMMENT;
+        } else {
+            result = super.getPartitionType(region, offset);
+        }
+
+        return result;
+    }
+
+}

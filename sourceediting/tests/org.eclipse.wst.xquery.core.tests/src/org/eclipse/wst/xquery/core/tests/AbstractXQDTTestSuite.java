63039fa520424dffb88e0c59bf2d51f004df36f7
Gabriel Petrovay <gabipetrovay@gmail.com>
2010-11-26 14:27:53 +0100

    Added java editor and pde settings to test plugins; added the zorba test plugin to the main pol file

---
+++ sourceediting/tests/org.eclipse.wst.xquery.core.tests/src/org/eclipse/wst/xquery/core/tests/AbstractXQDTTestSuite.java
@@ -0,0 +1,9 @@
+package org.eclipse.wst.xquery.core.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+public abstract class AbstractXQDTTestSuite {
+
+}

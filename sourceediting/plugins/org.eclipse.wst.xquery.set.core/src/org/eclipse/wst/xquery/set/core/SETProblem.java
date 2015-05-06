ea45a5f4a2e8215728af3db2d86b3f4eee234ee8
Matt Crosier <matt.crosier@marklogic.com>
2015-05-05 22:54:47 -0400

    Remove SET (Non-MarkLogic Support for XQuery/XPath)

--- sourceediting/plugins/org.eclipse.wst.xquery.set.core/src/org/eclipse/wst/xquery/set/core/SETProblem.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.wst.xquery.set.core;
-
-public class SETProblem {
-
-    public static final int INVALID_LOGICAL_URI = 1;
-    public static final int INVALID_MODULE_NAME = 2;
-}
